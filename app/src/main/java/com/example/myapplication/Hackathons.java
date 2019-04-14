929b34ad15471e420a3263886dc8776e5c03f702
Shefali Qamar <sqamar@ucsc.edu>
2019-04-13 18:47:18 -0700

    Scraping works!!!

--- app/src/main/java/com/example/myapplication/Hackathons.java
+++ app/src/main/java/com/example/myapplication/Hackathons.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 public class Hackathons extends AppCompatActivity {
-
+    TextView texx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,26 +29,36 @@ public class Hackathons extends AppCompatActivity {
             }
         });
 
-        // Button between Hackathons and Friends
-        Button friends = findViewById(R.id.friends);
-        friends.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(Hackathons.this, Friends.class));
-                finish();
+
+        texx = (TextView) findViewById(R.id.scrapeBox1);
+        Button but = (Button) findViewById(R.id.scrapeButton);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new doit().execute();
             }
         });
+    }
 
-        // Button between Hackathons and Resources
-        Button resources = findViewById(R.id.resources);
-        friends.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Log.d("katrin", "pressed resources button");
-                startActivity(new Intent(Hackathons.this, Resources.class));
-                Log.d("katrin", "after startactivity");
-                finish();
-            }
-        });
+    public class doit extends AsyncTask<Void,Void,Void> {
+        String words;
+        @Override
+        protected Void doInBackground(Void...params) {
+            try {
+                Document doc = Jsoup.connect("https://mlh.io/seasons/na-2019/events").get();
+                words = doc.text();
 
 
+            } catch(Exception e){e.printStackTrace();}
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            texx.setText(words);
+        }
     }
 }
